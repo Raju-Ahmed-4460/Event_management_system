bd08a97357aaf174ea2e3317617cfb8102b32362
Raju Ahmed <rajur20m@gmail.com>
2026-04-12 01:52:28 +0600

    dynamic dashboard

--- event/urls.py
+++ event/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from event.views import Event_form,Category_form,Participent_form
+from event.views import Event_form,Category_form,Participent_form,dashboard
 
 urlpatterns = [
     path("create_event/",Event_form),
-    path("create_category",Category_form),
+    path("create_category/",Category_form),
     path("participent_create/",Participent_form),
+    path("dashboard/",dashboard,name="dashboard"),
     
 ]
--- event/views.py
+++ event/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from event.form import EventModelForm,CategoryModelForm,ParticipantModelForm
+from event.models import Event,Participant,Category
+from  datetime import date,time
 
 
 # Create your views here.
@@ -57,6 +59,60 @@ def Participent_form(request):
         "form":form
     }
     return render(request,"dashboard/form.html",context)
+
+
+
+
+def dashboard(request):
+    type=request.GET.get('type','all')
+    
+    today = date.today()
+    base_query=Event.objects.select_related('category')\
+                           .prefetch_related('participants')
+    events = base_query.all()
+
+    total_events = base_query.count()
+    total_participants = Participant.objects.count()
+
+    upcoming = base_query.filter(date__gt=today).count()
+    past = base_query.filter(date__lt=today).count()
+
+    today_events = base_query.filter(date=today)
+
+    
+
+
+    
+
+    if type=='total_events':
+        events=base_query.all()
+
+    elif type=='past':
+        events=base_query.filter(date__lt=today)
+    
+    elif type=='upcoming':
+        events=base_query.filter(date__gt=today)
+    elif type=='total_participants':
+        events=Participant.objects.all()
+    elif type=='all':
+        events=base_query.all()
+
+
+
+
+
+    context = {
+        'events': events,
+        'total_events': total_events,
+        'total_participants': total_participants,
+        'upcoming': upcoming,
+        'past': past,
+        'today_events': today_events,
+    }
+    
+
+
+    return render(request, 'dashboard/dashboard.html', context)
 
     
 
