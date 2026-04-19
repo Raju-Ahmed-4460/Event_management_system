2565b8fd9892cb9314a256273e8c1282677ba6f5
Raju Ahmed <rajur20m@gmail.com>
2026-04-19 22:03:33 +0600

    Add email signals for Participents

--- event/models.py
+++ event/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save,post_delete,pre_save,m2m_changed
+from django.dispatch import receiver
+from django.core.mail import send_mail
 
 # Create your models here.
 class Category(models.Model):
@@ -29,4 +32,22 @@ class Participant(models.Model):
     def __str__(self):
         return self.name
 
-    
+
+@receiver(m2m_changed, sender=Participant.events.through)
+def notify_participant_for_event(sender, instance, action, pk_set, **kwargs):
+
+    if action == "post_add":
+        all_mail = [instance.email]  
+        all_event=""
+        for e in instance.events.all():
+            all_event+=e.name+" ,"
+        all_event = all_event.rstrip(", ")
+
+        send_mail(
+            "Event message",
+            f"You have been selected for this{all_event}",
+            "rajur20m@gmail.com",
+            all_mail,
+        )
+
+   
--- event_management/settings.py
+++ event_management/settings.py
@@ -147,3 +147,17 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'rajur20m@gmail.com'
+EMAIL_HOST_PASSWORD = "hyzd moll ggnr ztad"
+
+
+
+# hyzd moll ggnr ztad
