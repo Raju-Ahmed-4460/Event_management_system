ade34080f23ad54fc01fdf2eebb758b7f7b331a4
Raju Ahmed <rajur20m@gmail.com>
2026-05-02 04:09:31 +0600

    add home

--- event_management/urls.py
+++ event_management/urls.py
@@ -3,15 +3,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🚀 Event Management System is Running!")
 
 urlpatterns = [
+    path("", home),  
     path("admin/", admin.site.urls),
     path("event/", include('event.urls')),
     path("user/", include('user.urls')),
     path("core/", include('core.urls')),
 ]
 
-# Only enable debug toolbar in DEBUG mode
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
