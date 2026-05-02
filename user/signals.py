fe19c29d637947b5a71d3ff3149eca8ae8ab7ed5
Raju Ahmed <rajur20m@gmail.com>
2026-05-02 16:44:10 +0600

    change signals in previous one

--- user/signals.py
+++ user/signals.py
@@ -1,35 +1,38 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import post_save,post_delete,pre_save,m2m_changed
 from django.dispatch import receiver
 from django.core.mail import send_mail
-from event.models import Participant, Event
-from threading import Thread
+from django.contrib.auth.models import User,Group
+from django.contrib.auth.tokens import default_token_generator
+from django.conf import settings
+from django.core.mail import send_mail
 
 
-def send_event_email(email, subject, message):
-    try:
-        send_mail(
-            subject,
-            message,
-            "rajur20m@gmail.com",
-            [email],
-            fail_silently=True
-        )
-    except Exception as e:
-        print("Email error:", e)
+
+@receiver(post_save,sender=User)
+
+def send_activation_email(sender,instance,created,**kwargs):
+    if created:
+        token=default_token_generator.make_token(instance)
+        activation_urls=f"{settings.FRONTEND_URL}/user/activate/{instance.id}/{token}/"
+
+        subject='active your account'
+        message=f"Hi {instance.username},\n\n activate your account by clicking the link below\n{activation_urls}\n\n Thank You"
+        recipent_list=[instance.email]
+        try:
+            send_mail(subject,message,settings.EMAIL_HOST_USER,recipent_list)
+        except Exception as e:
+            print(f"failed to send mail {instance.email}:{str(e)}")
 
 
-@receiver(m2m_changed, sender=Participant.events.through)
-def notify_participant_for_event(sender, instance, action, pk_set, **kwargs):
 
-    if action == "post_add":
 
-        event_names = Event.objects.filter(id__in=pk_set).values_list("name", flat=True)
-        all_event = ", ".join(event_names)
+@receiver(post_save,sender=User)
+def assign_role(sender,created,instance,**kwargs):
+    if created:
+        user_group,created=Group.objects.get_or_create(name='User')
+        instance.groups.add(user_group)
+        instance.save()
 
-        subject = "Event Message"
-        message = f"You have been selected for: {all_event}"
 
-        Thread(
-            target=send_event_email,
-            args=(instance.email, subject, message)
-        ).start()
+
+        
