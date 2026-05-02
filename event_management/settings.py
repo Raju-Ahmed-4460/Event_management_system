9f8ecc29d9363179390e7882c3d5e5fe8686b351
Raju Ahmed <rajur20m@gmail.com>
2026-05-03 01:22:11 +0600

    fix the code for deply

--- event_management/settings.py
+++ event_management/settings.py
@@ -156,14 +156,16 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR /'media'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
